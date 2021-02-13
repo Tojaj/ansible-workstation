d04958b8aff27e8c0d31636211c039845ec2106f
Tomas Mlcoch <xtojaj@gmail.com>
2021-02-13 20:42:28 +0100

    Add Firefox settings

---
+++ roles/common/files/firefox-prefs-tojaj.js
@@ -0,0 +1,12 @@
+/* Belongs to: /usr/lib64/firefox/defaults/pref/ */
+
+/* Common */
+
+pref("browser.download.useDownloadDir", false);
+pref("browser.sessionstore.interval", 600000);
+
+/* Hardware acceleration via VAAPI */
+pref("gfx.webrender.all", true);
+pref("layers.acceleration.force-enabled", true);
+pref("media.ffmpeg.vaapi.enabled", true);
+pref("media.ffvpx.enabled", false);
