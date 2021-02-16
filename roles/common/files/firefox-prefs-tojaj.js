a5200358a26af771d0d7d3ab41ca1992b5c218ab
Tomas Mlcoch <tmlcoch@redhat.com>
2021-02-16 15:22:53 +0100

    Add link a arch linux wiki

--- roles/common/files/firefox-prefs-tojaj.js
+++ roles/common/files/firefox-prefs-tojaj.js
@@ -6,6 +6,9 @@ pref("browser.download.useDownloadDir", false);
 pref("browser.sessionstore.interval", 600000);
 
 /* Hardware acceleration via VAAPI */
+/* More info:
+ * https://wiki.archlinux.org/index.php/Firefox#Hardware_video_acceleration
+ */
 pref("gfx.webrender.all", true);
 pref("layers.acceleration.force-enabled", true);
 pref("media.ffmpeg.vaapi.enabled", true);
